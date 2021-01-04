292ad217dfa491986472b6e4c34a694fcc263ac5
Bradley Pawlow <bradleypawlow2023@u.northwestern.edu>
2021-01-04 16:10:44 -0600

    final commit

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function playAudio(sound){
     var audio = new Audio(sound);
     audio.play()
 }
+
+//document.onclick = playAudio;
+
+//const crash_sound = document.getElementById("crash");
+
+//document.getElementsByClassName("k drum")[0].addEventListener("click", playAudio('sounds/crash.mp3'));
